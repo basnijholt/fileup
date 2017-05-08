373e8c3be386f9411dbc0695e8def0a61ca97c62
Bas Nijholt <basnijholt@gmail.com>
2017-05-08 14:42:45 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fileup',
-      version='0.1.1',
+      version='0.2.0',
       description='Easily upload files to an FTP-server and get back the url.',
       url='https://github.com/basnijholt/fileup',
       author='Bas Nijholt',
