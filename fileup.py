90fedaf7b4899ddb704818b3569e0899a913acdb
Bas Nijholt <basnijholt@gmail.com>
2020-06-12 14:33:54 +0200

    add pre-commit

--- fileup.py
+++ fileup.py
@@ -2,7 +2,7 @@
 # -*-Python-*-
 
 import argparse
-import base64
+import contextlib
 import datetime
 import ftplib
 import os
@@ -20,13 +20,13 @@ def get_valid_filename(s):
     >>> get_valid_filename("john's portrait in 2004.jpg")
     'johns_portrait_in_2004.jpg'
     """
-    s = s.strip().replace(' ', '_')
-    return re.sub(r'(?u)[^-\w.]', '', s)
+    s = s.strip().replace(" ", "_")
+    return re.sub(r"(?u)[^-\w.]", "", s)
 
 
 def read_config():
     # Read the config
-    with open(os.path.expanduser('~/.config/fileup/config'), 'r') as f:
+    with open(os.path.expanduser("~/.config/fileup/config")) as f:
         """Create a config file at ~/.config/fileup/config with the
         following information and structure:
             example.com
@@ -34,21 +34,23 @@ def read_config():
             my_user_name
             my_difficult_password
         """
-        base_url, base_folder, folder, user, pw = [s.replace('\n', '') for s in f.readlines()]
+        base_url, base_folder, folder, user, pw = [
+            s.replace("\n", "") for s in f.readlines()
+        ]
     return base_url, base_folder, folder, user, pw
 
 
 def remove_old_files(ftp, today):
     # Remove all files that are past the limit
-    files = [f for f in ftp.nlst() if '_delete_on_' in f]
-    file_dates = [f.rsplit('_delete_on_', 1) for f in files]
+    files = [f for f in ftp.nlst() if "_delete_on_" in f]
+    file_dates = [f.rsplit("_delete_on_", 1) for f in files]
     for file_name, date in file_dates:
-        rm_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+        rm_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
         if rm_date < today:
-            print('removing "{}" because the date passed'.format(file_name))
+            print(f'removing "{file_name}" because the date passed')
             try:
                 ftp.delete(file_name)
-            except:
+            except Exception:
                 # File didn't exist anymore for some reason...
                 pass
             ftp.delete(file_name + "_delete_on_" + date)
@@ -56,21 +58,30 @@ def remove_old_files(ftp, today):
 
 def main():
     # Get arguments
-    description = ["Publish a file. \n \n",
-                   "Create a config file at ~/.config/fileup/config with the following information and structure:\n",
-                   "example.com",
-                   "base_folder",
-                   "file_up_folder",
-                   "my_user_name",
-                   "my_difficult_password"]
+    description = [
+        "Publish a file. \n \n",
+        "Create a config file at ~/.config/fileup/config with the following information and structure:\n",
+        "example.com",
+        "base_folder",
+        "file_up_folder",
+        "my_user_name",
+        "my_difficult_password",
+    ]
 
-    parser = argparse.ArgumentParser(description='\n'.join(description),
-                                     formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('fname', type=str)
-    parser.add_argument('-t', '--time', type=int, default=90, 
-        help='If time is 0 the file will never be deleted, default is 90 days.')
-    parser.add_argument('-d', '--direct', action='store_true')
-    parser.add_argument('-i', '--img', action='store_true')
+    parser = argparse.ArgumentParser(
+        description="\n".join(description),
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument("fname", type=str)
+    parser.add_argument(
+        "-t",
+        "--time",
+        type=int,
+        default=90,
+        help="If time is 0 the file will never be deleted, default is 90 days.",
+    )
+    parser.add_argument("-d", "--direct", action="store_true")
+    parser.add_argument("-i", "--img", action="store_true")
     args = parser.parse_args()
     fname = os.path.abspath(os.path.expanduser(args.fname))
 
@@ -91,48 +102,48 @@ def main():
     # Delete first if file already exists, it could happen that there is
     # already a file with a specified deletion date, these should be removed.
     for f in ftp.nlst():
-        if f.startswith(fname_base) and '_delete_on_' in f:
+        if f.startswith(fname_base) and "_delete_on_" in f:
             ftp.delete(f)
 
     if args.time != 0:  # could be negative (used for debugging).
         remove_on = today + datetime.timedelta(days=args.time)
-        fname_date = fname_base + '_delete_on_' + str(remove_on)
+        fname_date = fname_base + "_delete_on_" + str(remove_on)
         with tempfile.TemporaryFile() as f:
-            print('upload ' + fname_date)
-            ftp.storbinary('STOR {0}'.format(fname_date), f)
+            print("upload " + fname_date)
+            ftp.storbinary(f"STOR {fname_date}", f)
 
     # Upload and open the actual file
-    with open(fname, 'rb') as f:
-        ftp.storbinary('STOR {0}'.format(fname_base), f)
-        print('upload ' + fname_base)
+    with open(fname, "rb") as f:
+        ftp.storbinary(f"STOR {fname_base}", f)
+        print("upload " + fname_base)
         ftp.quit()
 
     # Create URL
     if folder:
-        url = '{}/{}/{}'.format(base_url, folder, fname_base)
+        url = f"{base_url}/{folder}/{fname_base}"
     else:
-        url = '{}/{}'.format(base_url, fname_base)
+        url = f"{base_url}/{fname_base}"
 
     if args.direct:
         # Returns the url as is.
-        url = 'http://' + url
+        url = "http://" + url
     elif args.img:
-        url = '![](http://{})'.format(url)
-    elif fname.endswith('.ipynb'):
+        url = f"![](http://{url})"
+    elif fname.endswith(".ipynb"):
         # Return the url in the nbviewer
-        url = 'http://nbviewer.jupyter.org/url/' + url + '?flush_cache=true'
+        url = "http://nbviewer.jupyter.org/url/" + url + "?flush_cache=true"
     else:
-        url = 'http://' + url
+        url = "http://" + url
 
     # Put a URL into clipboard only works on OS X
-    try:
-        process = subprocess.Popen('pbcopy', env={'LANG': 'en_US.UTF-8'},
-                                   stdin=subprocess.PIPE)
-        process.communicate(url.encode('utf-8'))
-    except:
-        pass
+    with contextlib.suppress(Exception):
+        process = subprocess.Popen(
+            "pbcopy", env={"LANG": "en_US.UTF-8"}, stdin=subprocess.PIPE
+        )
+        process.communicate(url.encode("utf-8"))
 
-    print('Your url is: ', url)
+    print("Your url is: ", url)
+
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='fileup',
-      version='0.2.1',
-      description='Easily upload files to an FTP-server and get back the url.',
-      url='https://github.com/basnijholt/fileup',
-      author='Bas Nijholt',
-      license='BSD 3-clause',
-      py_modules=["fileup"],
-      entry_points={'console_scripts': ['fu=fileup:main']}
-      )
+setup(
+    name="fileup",
+    version="0.2.1",
+    description="Easily upload files to an FTP-server and get back the url.",
+    url="https://github.com/basnijholt/fileup",
+    author="Bas Nijholt",
+    license="BSD 3-clause",
+    py_modules=["fileup"],
+    entry_points={"console_scripts": ["fu=fileup:main"]},
+)
