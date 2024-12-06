35f7c26ba72494c9e29be812d0270f09a1f0c499
Bas Nijholt <bas@nijho.lt>
2024-12-05 22:23:55 -0800

    no ticks

--- fileup.py
+++ fileup.py
@@ -307,7 +307,6 @@ def fileup(
 DESCRIPTION = [
     "Publish a file.\n\n",
     "Create a config file at ~/.config/fileup/config.ini with the following structure:\n",
-    "```",
     "[default]",
     "protocol = ftp  # or scp",
     "hostname = example.com  # or the Host from your ~/.ssh/config",
@@ -325,7 +324,6 @@ DESCRIPTION = [
     "# If using SSH config, no need for these",
     "private_key =",
     "password =",
-    "```",
 ]
 
 
